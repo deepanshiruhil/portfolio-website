123f3764c9046b8c39c054078397dca7925b7f11
deepanshiruhil <178875578+deepanshiruhil@users.noreply.github.com>
2026-07-31 08:08:01 +0530

    Remove ThemeProvider wrapper from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { ThemeProvider } from "./components/ThemeProvider"
 import Desktop from "./components/Desktop"
 
 export default function Home() {
-  return (
-    <ThemeProvider>
-      <Desktop />
-    </ThemeProvider>
-  )
+  return <Desktop />
 }
